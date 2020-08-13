8a78317e4d3d0be673fcfd7f1f4134845bf1937e
SebastianOderland <sebastian.oderland@gmail>
2020-08-13 17:20:54 +0200

    update

---
+++ auto.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+token=$(git config --global github.token)
+
+read -p "Commit description: " desc
+read -p "Version: " version
+
+generate_post_data()
+{
+  cat <<EOF
+{
+  "tag_name": "v_$version",
+  "target_commitish": "$branch",
+  "name": "v_$version",
+  "body": "$desc",
+  "draft": false,
+  "prerelease": false
+}
+EOF
+}
+
+git add .
+
+git commit -m "$desc"
+git push origin master
+curl --data "$(generate_post_data)" "https://api.github.com/repos/SebastianOderland/zypher_backtester/releases?access_token=$token"
+
+python3 setup.py sdist "$version"
+python3 -m twine upload dist/*
